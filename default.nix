b3730b4aad25ad4206bc20f1229adc3f853d843a
Valeriy Litkovskyy <vlr.ltkvsk@protonmail.com>
2021-03-22 09:43:07 +0100

    default:nix update hash

--- default.nix
+++ default.nix
@@ -2,12 +2,12 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "rimer";
-  version = "0.1.0";
+  version = "0.1.1";
   src = pkgs.fetchFromGitHub {
     owner = "xFA25E";
     repo = pname;
     rev = version;
-    sha256 = "12cspj036kma9shm0124cvdbi4iaxjwiaw4x3mngpbr3qyh1xrd6";
+    sha256 = "0vqxkq2sg8ac44aibarvrgjm09966m35fl58s865vqkzd92ilhvl";
   };
   cargoSha256 = "0qxmy2gynk5r4327mfsw0ab7y0g7vx1qarcl0i7d5i5brvykijxv";
   meta = with pkgs.lib; {
